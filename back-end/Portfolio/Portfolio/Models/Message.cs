f416b6e7f0ba5d97b0a498369f739fe5cee21637
shawn <2017003399@student.sit.ac.nz>
2020-08-11 00:02:25 +1200

    update

---
+++ back-end/Portfolio/Portfolio/Models/Message.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Portfolio.Models
+{
+    public class Message
+    {
+        [Required(ErrorMessage = "Please enter your first name.")]
+        [DataType(DataType.Text)]
+        [Display(Name = "First Name:")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "Please enter your lastname.")]
+        [DataType(DataType.Text)]
+        [Display(Name = "Last Name:")]
+        public string LastName { get; set; }
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Gender:")]
+        public string Gender { get; set; }
+        [Required(ErrorMessage = "Please enter your email.")]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Your Email Address:")]
+        public string Email { get; set; }
+        [DataType(DataType.PhoneNumber)]
+        [Display(Name = "Your Mobile Number (if possible):")]
+        public string Mobile { get; set; }
+        [DataType(DataType.MultilineText)]
+        [Display(Name = "Comments:")]
+        public string ExtraComment { get; set; }
+    }
+}
---
+++ back-end/Portfolio/Portfolio/wwwroot/js/site.js
@@ -0,0 +1,33 @@
+﻿$("#Top").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 1000);
+});
+
+$(window).on('load', UpDownArrowsSwitcher());
+$(window).resize(function () {
+    UpDownArrowsSwitcher();
+});
+
+function UpDownArrowsSwitcher() {
+    if (ToDisableNavigationArrows()) {
+        $('#Top').css({ display: 'none' });
+        $('#furtherInfo').css({ display: 'none' });
+    }
+    else {
+        $('#Top').css({ display: 'block' });
+        $('#furtherInfo').css({ display: 'block' });
+        window.scroll(0, $(document).scrollTop() + 1);
+        window.scroll(0, $(document).scrollTop() - 1);
+    }
+}
+
+function ToDisableNavigationArrows() {
+    return $(window).height() < 768 || $(window).width() < 768 || ($(window).width() < 768 && $('#Contact').parent().find('div').hasClass('border-div-active'));
+}
+
+
+//google analytics plugin
+
+window.dataLayer = window.dataLayer || [];
+function gtag() { dataLayer.push(arguments); }
+gtag('js', new Date());
+gtag('config', 'UA-171792787-1');
