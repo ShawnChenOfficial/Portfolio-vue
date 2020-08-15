58b94e3f2acffb8cf3191b81e37f45fd2d224522
shawn <2017003399@student.sit.ac.nz>
2020-08-15 13:17:03 +1200

    update

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -7,11 +7,17 @@ import "../node_modules/jquery/dist/jquery.min.js";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 import "./assets/css/site.css";
 import axios from "axios";
+import VueAnalytics from "vue-analytics";
 
 axios.defaults.headers.get["Content-Type"] = "application/json;charset=UTF-8";
 axios.defaults.headers.post["Content-Type"] = "application/json;charset=UTF-8";
 
 Vue.config.productionTip = false;
+
+Vue.use(VueAnalytics, {
+  id: "Google-Analytics-ID",
+  router
+});
 
 new Vue({
   router,
